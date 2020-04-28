46a67917a6a1a8a300e4a4448c7d0d8f14bb2b42
Pietrek xD <pietrek.j@gmail.com>
2020-04-28 11:45:26 +0200

    threads

--- providers.py
+++ providers.py
@@ -13,10 +13,15 @@ def process_melissa():
     errors = 0
     with JSONStorage('j') as stor:
         # stor.clear_errors()
-        for n, v in map_scrape_get(
-                'https://www.melissa.com/v2/lookups/zip4/zip4/?zip4={}&tbl=mak&fmt=json',
-                stor.filter_items(tqdm(rr))
-        ):
+        total_len = len(rr)
+        rr = tuple(stor.filter_items(rr))
+        filtered_len = total_len - len(rr)
+        counter = tqdm(map_scrape_get(
+            'https://www.melissa.com/v2/lookups/zip4/zip4/?zip4={}&tbl=mak&fmt=json',
+            rr
+        ), total=total_len)
+        counter.update(filtered_len)
+        for n, v in counter:
             if ' ' not in n:
                 try:
                     v = json.loads(v)
--- scrape.py
+++ scrape.py
@@ -1,12 +1,33 @@
+from concurrent.futures.thread import ThreadPoolExecutor
+from functools import partial
+
 import requests
 
 from setting import get_headers, get_proxies
 
 
-def map_scrape_get(url_template, items):
-    headers = get_headers()
-    proxies = get_proxies()
-    for item in items:
-        r = requests.get(url_template.format(*item.split('#')), headers=headers, proxies=proxies)
+class RequestHandler:
+    def __init__(self, headers=(), proxies=()):
+        self._headers = dict(headers) or get_headers()
+        self._proxies = dict(proxies) or get_proxies()
+
+    def get(self, url):
+        r = requests.get(url, headers=self._headers, proxies=self._proxies)
         r.raise_for_status()
-        yield item, r.text
+        return r
+
+
+def _get_process_item(req: RequestHandler, url_template, item):
+    url = url_template.format(*item.split('#'))
+    r = req.get(url)
+    r.raise_for_status()
+    return item, r.text
+
+
+def map_scrape_get(url_template, items, threads=4):
+    req = RequestHandler()
+    with ThreadPoolExecutor(threads) as pool:
+        yield from pool.map(
+            partial(_get_process_item, req, url_template),
+            items
+        )
